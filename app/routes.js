1b1492ccd82006c5833ed942ca182c37330b6947
Adam Cooper <amcooper@gmail.com>
2017-11-04 00:39:22 -0400

    Get requests working.

--- app/routes.js
+++ app/routes.js
@@ -2,116 +2,14 @@ const db = require( '../config/database' );
 
 module.exports = function( app ) {
 	app.get( '/api/sightings', ( request, response, next ) => {
-		db.serialize( function() { // refactor to es6
-	    db.all( 'SELECT * FROM sightings', ( err, rows ) => {
-	    	if ( err ) {
-	    		return next( err );
-	    	}
-	    	return response.json( rows );
-	    });
-	  });
+		db.query( 'SELECT * FROM sightings')
+		  .then( res => response.json( res.rows ))
+		  .catch( e => console.error( e.stack ));
 	});
 
   app.get( '/api/sightings/:id', (request, response, next ) => {
-    db.serialize( function() {
-    	db.get( 'SELECT * FROM sightings WHERE id = ?', request.params.id, ( err, row ) => {
-    		if ( err ) {
-    			return next( err );
-    		}
-    		return response.json( row );
-    	});
-    });
+    db.query( 'SELECT * FROM sightings WHERE id = $1', [ request.params.id ])
+      .then( res => response.json( res.rows[0] ))
+      .catch( e => console.error( e.stack ));
   });
-
-  app.post( '/api/sightings', ( request, response, next ) => {
-  	// response.send( "successful post." );
-  	// db.serialize( function() {
-		// db.run( `INSERT INTO sightings ( celebrity, stalker, location, comment ) VALUES ( ${request.body.celebrity}, ${request.body.stalker}, ${request.body.location}, ${request.body.comment} )` );
-		db.run( "INSERT INTO sightings ( celebrity, stalker, location, comment ) VALUES ( 'test1', 'test2', 'test3','test4' )" );
-		response.status( 200 );
-		//  });
-  });  	
-//    db.serialize( function() {
-//    	let statement = db.prepare( 'INSERT INTO sightings ( celebrity, stalker, date, location, comment ) VALUES ( ?, ?, ?, ?, ? )');
-//    	console.log( 'db.prepare' ); // debug
-//    	console.log( `Request params: ${ request.params.celebrity }` ); // debug
-//    	statement.run( request.params.celebrity, request.params.stalker, request.params.date, request.params.location, request.params.comment, ( err ) => {
-//    		if ( err ) { return next( err ); }
-//    		console.log( 'statement.run' ); // debug
-//    	});
-//    	statement.finalize();
-//  		console.log( `Request params: ${ request.params.celebrity }` ); // debug
-//  		console.log( 'Record created.' );
-//  		return response.status(200);
-//    });
-//  });
 };
-
-  // 	db.run( "INSERT INTO sightings ( celebrity, stalker, location, comment ) VALUES ( ?, ?, ?, ? )", request.body.celebrity, request.body.stalker, request.body.location, request.body.comment, function( err ) {
-  	// 		if ( err ) { 
-  	// 			console.error( err );
-  	// 			return next( err ); 
-  	// 		}
-  	// 	});
-  	// 	// return response.status( 200 );
-  	// });
-  	// response.json( request.body.celebrity );
-   //  db.serialize( function() {
-   //  	let statement = db.prepare( 'INSERT INTO sightings ( celebrity, stalker, date, location, comment ) VALUES ( ?, ?, ?, ?, ? )' );
-   //  	console.log( 'db.prepare' ); // debug
-   //  	console.log( `Request params: ${ Object.values( request.body )}` ); // debug
-   //  	statement.run( request.body.celebrity, request.body.stalker, request.body.date, request.body.location, request.body.comment, function( err ) {
-   //  		if ( err ) {
-   //  			return next( err );
-   //  		}
-   //  	}).finalize();
-  	// 	console.log( 'statement.run' ); // debug
-  	// 	console.log( `Request params: ${ request.body.celebrity }` ); // debug
-  	// 	console.log( 'Record created.' );
-
-
-// id INTEGER PRIMARY KEY,
-// celebrity VARCHAR(80),
-// stalker VARCHAR(80),
-// date TIMESTAMP,
-// location VARCHAR(80),
-// comment VARCHAR(255)
-
-// const Sighting = require('./models/sighting');
-
-// function getSightings( response ) {
-//    Sighting.forge()
-//      .fetch()
-//      .then( function( sightings ) {
-//      	  response.json( sightings );
-//      })
-//      .catch( function( error ) {
-//      	  response.send( error );
-//      });
-// }
-
-// module.exports = function( app ) {
-// 	app.get('/api/sightings', function( request, response ) {
-// 		getSightings( response );
-// 		// TODO start a branch with SQL code in pg clients
-// 		// const res = await client.query('SELECT * FROM sightings')
-// 	});
-
-// 	app.post('/api/sightings', function( request, response ) {
-// 		Sighting.forge({
-// 			celeb: request.body.celeb,
-// 			// csdlc
-// 			stalker: request.body.stalker,
-// 			dateTime: request.body.dateTime,
-// 			location: request.body.location,
-// 			comment: request.body.comment
-// 		})
-// 		  .save()
-// 		  .then( function( sighting ) {
-// 		  	response.json( sighting );
-// 		  })
-// 		  .catch( function( error ) {
-// 		  	response.send( error );
-// 		  });
-// 	});
-// };
--- config/database.js
+++ config/database.js
@@ -1,35 +1,16 @@
-const sqlite3 = require( 'sqlite3' ).verbose();
-const db = new sqlite3.Database( './react-stalker-api.sqlite3' );
+const { Pool } = require ('pg');
 
-module.exports = db;
-
-// const knex = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     database : 'react-stalker-api'
-//   }	
-// });
-
-// module.exports = {
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     database : 'react-stalker-api'
-//   }	
-// };
-
-// const { Pool } = require ('pg');
-
-// const pool = new Pool({
-//   user: 'adamcooper',
-//   host: '127.0.0.1',
-//   database: 'react-stalker-api',
-//   // password: 'secretpassword'
-// });
+const pool = new Pool({
+  user: 'adamcooper',
+  host: '127.0.0.1',
+  database: 'react_stalker',
+  // password: 'secretpassword'
+});
 
 // module.exports = {
 //   query: (text, params, callback) => {
 // 	return pool.query(text, params, callback);
 // 	}
 // };
+
+module.exports = pool;
