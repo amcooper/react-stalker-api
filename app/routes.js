f15ac231bfd2f4257b8b87fdf1445db824fe5108
Adam Cooper <amcooper@gmail.com>
2017-11-05 13:26:12 -0500

    Got POST request working.

--- app/routes.js
+++ app/routes.js
@@ -12,4 +12,14 @@ module.exports = function( app ) {
       .then( res => response.json( res.rows[0] ))
       .catch( e => console.error( e.stack ));
   });
+
+  app.post( '/api/sightings', ( request, response, next ) => {
+  	console.log( request ); // debug
+  	db.query( 'INSERT INTO sightings ( celebrity, stalker, date, location, comment ) VALUES ( $1, $2, $3, $4, $5 )', [ request.body.celebrity, request.body.stalker, request.body.date, request.body.location, request.body.comment ])
+  	  .then( res => {
+  	  	console.log( res.rows[0] );
+  	  	response.json( res.rows[0] );
+  	  })
+  	  .catch( e => console.error( e.stack ));
+  });
 };
