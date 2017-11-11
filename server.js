c2f2dd2490926477c201da2a65cc934acb82ae67
Adam Cooper <amcooper@gmail.com>
2017-11-10 22:29:45 -0500

    CORS middleware thing

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+// from https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./app/routes')( app );
 
 app.get('/', function( request, response ) { 
