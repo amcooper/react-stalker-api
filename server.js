788e6052b8e1679abc76a5ca814a2323993424ca
Adam Cooper <amcooper@gmail.com>
2017-11-19 12:02:38 -0500

    Allow Delete and Put

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 // from https://enable-cors.org/server_expressjs.html
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 require('./app/routes')( app );
 
-app.get('/', function( request, response ) { 
+app.get('/', function( request, response ) {
 	response.json('Slash route is go.');
 });
 
